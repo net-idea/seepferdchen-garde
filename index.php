1c50540e5c2a896c56fda1d2abc682ddb548326d
Adam Ibrom <adam@net-i.de>
2025-09-17 16:02:45 +0200

    feat: Remove Google Tag Manager

--- index.php
+++ index.php
@@ -59,14 +59,13 @@
       "sameAs": []
     }
     </script>
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PRDQMCR7');</script>
+
     <link href="/style/fonts.css" rel="stylesheet">
     <link href="/style/bootstrap.min.css" rel="stylesheet">
     <link href="/style/main.css" rel="stylesheet">
     <link href="/style/carousel.css" rel="stylesheet">
 </head>
 <body>
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PRDQMCR7" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
 <a class="visually-hidden-focusable" href="#content">Direkt zum Inhalt springen</a>
 
